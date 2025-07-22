358ba5b59d67508161401f014fbf73c3b048b9e8
Dierlisson Justiniano <dierlisson1@gmail.com>
2025-07-21 23:35:41 -0300

    Create card with async image from API

--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,11 +22,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.devspacecinenow.ui.theme.CineNowTheme
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +55,7 @@ class MainActivity : ComponentActivity() {
                             }
 
                         }else{
-                            Log.d("MainActivity", "Request Error: ${response.errorBody()?.string()}")
+                            Log.d("MainActivity", "Request Error :: ${response.errorBody()?.string()}")
                         }
                     }
 
@@ -53,7 +63,7 @@ class MainActivity : ComponentActivity() {
                         call: Call<MovieResponse?>,
                         t: Throwable
                     ) {
-                        Log.d("MainActivity", "Network Error: ${t.message}")
+                        Log.d("MainActivity", "Network Error :: ${t.message}")
                     }
 
                 })
@@ -62,14 +72,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyColumn {
-                        items(nowPlayingMovies){
-                            Text(text = it.title)
-
-                        }
+                    MovieList(
+                        nowPlayingMovies
+                    ){
+                        movieClicked ->
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun MovieList(
+    movieList: List<MovieDto>,
+    onClick:(MovieDto) -> Unit = {}
+    ) {
+    LazyRow {
+        items(movieList){
+            MovieItem(
+                movieDto = it,
+                onClick = onClick
+            )
+        }
+    }
+}
+
+@Composable
+fun MovieItem(
+    movieDto: MovieDto,
+    onClick:(MovieDto) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.clickable{
+            onClick.invoke(movieDto)
+        }
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .padding(end = 4.dp)
+                .width(120.dp)
+                .height(150.dp),
+            contentScale = ContentScale.Crop,
+            model = movieDto.posterFullPath,
+            contentDescription = "${movieDto.title} Poster image"
+            )
+    }
+
 }
